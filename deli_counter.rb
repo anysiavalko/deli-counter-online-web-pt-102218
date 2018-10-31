e39510c7930a4fe9d51ba4a0efec2d1ff77457fc
A. V <anysiavalko@gmail.com>
2018-10-31 02:29:07 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(deli)
   if deli.size == 0
     puts "The line is currently empty."
   else 
-    deli[0] = "The line is currently: #{katz_deli[0]} "
+    deli.each_with_index = "The line is currently: #{katz_deli[0]} "
     
     index = katz_deli.index() puts katz_deli.join(" #{index} ")
   end
