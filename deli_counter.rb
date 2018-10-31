ad7176faa21ed7477c7d7acce98f0c06194e6a91
A. V <anysiavalko@gmail.com>
2018-10-31 02:10:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,8 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
-  else return katz_deli.join(" , ")
+  else 
+    katz_deli[0] = "The line is currently: #{array[0]}
+    return katz_deli.join(" , ")
   end
 end
